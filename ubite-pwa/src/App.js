e65511b921c165d27182cc59bd917885c320d480
Jewel <jeweled2512@yahoo.com>
2024-11-09 23:45:46 -0500

    updated the

--- ubite-pwa/src/App.js
+++ ubite-pwa/src/App.js
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './frontend/Login'; // Adjust the path as necessary
 import ForgotPassword from './frontend/ForgotPassword'; // Import your Forgot Password component
 import Register from './frontend/Register'; // Import your Register component
-//import HomePage from 
+import HomePage from './frontend/HomePage';
 const App = () => {
     return (
-        <Routes> {/* Wrap your Route components in Routes */}
+        <Routes> 
             <Route path="/" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/register" element={<Register />} />
-             {/* Add other routes as needed */}
+            <Route path="/home-page" element={<HomePage />} />
         </Routes>
     );
 };
